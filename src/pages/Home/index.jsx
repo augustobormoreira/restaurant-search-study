268be995ec0471cdd9c5f865975bddf83712b209
gustim <augustobormoreira@gmail.com>
2025-06-07 11:33:08 -0300

    Sidebar search and logo implemented, slick-react added for carousel future implementation

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import TextField, { Input } from '@material/react-text-field'
+import MaterialIcon from '@material/react-material-icon'
 
-import { Container } from './styles'
+import logo from '../../assets/logo.svg'
 
-const Home = () => <Container>Hello World!</Container>
+import { Wrapper, Container, Search, Logo, Map, CarouselTitle } from './styles'
+
+const Home = () => {
+
+    const [inputValue, setInputValue] = useState('cachorro');
+    return (
+    <Wrapper>
+        <Container>
+            <Search>
+                <Logo src={logo} alt="Logo do restaurante"/>
+                <TextField
+                    label='Pesquisar restaurantes'
+                    outlined
+                    trailingIcon={<MaterialIcon role="button" icon="search"/>}
+                >
+                    <Input
+                    value={inputValue}
+                    onChange={(e) => setInputValue(e.target.value)} />
+                </TextField>
+                <CarouselTitle>Na sua área</CarouselTitle>
+            </Search>
+        </Container>
+        <Map/>
+    </Wrapper>
+    )
+}
+
+
 
 export default Home
